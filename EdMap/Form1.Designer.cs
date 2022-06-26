65272ffbd767a5ed626c1a9db31d1f65db1cea58
Orkii <arkasha27a@gmail.com>
2022-06-26 11:58:04 +0500

    Some control move

--- EdMap/Form1.Designer.cs
+++ EdMap/Form1.Designer.cs
@@ -26,6 +26,7 @@ namespace EdMap {
         private void InitializeComponent() {
             this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.label1 = new System.Windows.Forms.Label();
             this.LoadCapacityNUD = new System.Windows.Forms.NumericUpDown();
@@ -36,9 +37,10 @@ namespace EdMap {
             this.treeView1 = new System.Windows.Forms.TreeView();
             this.label3 = new System.Windows.Forms.Label();
             this.getPathButton = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.loadPanel = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.LoadCapacityNUD)).BeginInit();
+            this.loadPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // gMapControl1
@@ -52,8 +54,7 @@ namespace EdMap {
             this.gMapControl1.GrayScaleMode = false;
             this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.gMapControl1.LevelsKeepInMemory = 5;
-            this.gMapControl1.Location = new System.Drawing.Point(153, 8);
-            this.gMapControl1.Margin = new System.Windows.Forms.Padding(2);
+            this.gMapControl1.Location = new System.Drawing.Point(230, 12);
             this.gMapControl1.MarkersEnabled = true;
             this.gMapControl1.MaxZoom = 2;
             this.gMapControl1.MinZoom = 2;
@@ -67,7 +68,7 @@ namespace EdMap {
             this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.gMapControl1.ShowTileGridLines = false;
-            this.gMapControl1.Size = new System.Drawing.Size(631, 523);
+            this.gMapControl1.Size = new System.Drawing.Size(1066, 935);
             this.gMapControl1.TabIndex = 0;
             this.gMapControl1.Zoom = 0D;
             this.gMapControl1.OnMapClick += new GMap.NET.WindowsForms.MapClick(this.gMapControl1_OnMapClick);
@@ -77,8 +78,7 @@ namespace EdMap {
             // 
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.progressBar1);
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.LoadCapacityNUD);
             this.panel1.Controls.Add(this.storageButtonAdd);
@@ -88,41 +88,52 @@ namespace EdMap {
             this.panel1.Controls.Add(this.treeView1);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.getPathButton);
-            this.panel1.Location = new System.Drawing.Point(9, 8);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Location = new System.Drawing.Point(14, 12);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(140, 522);
+            this.panel1.Size = new System.Drawing.Size(210, 933);
             this.panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 0);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 20);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "label";
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(3, 171);
+            this.progressBar1.Location = new System.Drawing.Point(3, 25);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(134, 23);
+            this.progressBar1.Size = new System.Drawing.Size(202, 35);
             this.progressBar1.TabIndex = 13;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(5, 91);
+            this.label1.Location = new System.Drawing.Point(4, 135);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.Size = new System.Drawing.Size(159, 20);
             this.label1.TabIndex = 12;
             this.label1.Text = "Грузоподъемность:";
             // 
             // LoadCapacityNUD
             // 
-            this.LoadCapacityNUD.Location = new System.Drawing.Point(5, 110);
+            this.LoadCapacityNUD.Location = new System.Drawing.Point(3, 160);
+            this.LoadCapacityNUD.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.LoadCapacityNUD.Name = "LoadCapacityNUD";
-            this.LoadCapacityNUD.Size = new System.Drawing.Size(132, 20);
+            this.LoadCapacityNUD.Size = new System.Drawing.Size(202, 26);
             this.LoadCapacityNUD.TabIndex = 11;
             // 
             // storageButtonAdd
             // 
-            this.storageButtonAdd.Location = new System.Drawing.Point(2, 65);
-            this.storageButtonAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.storageButtonAdd.Location = new System.Drawing.Point(3, 100);
             this.storageButtonAdd.Name = "storageButtonAdd";
-            this.storageButtonAdd.Size = new System.Drawing.Size(136, 21);
+            this.storageButtonAdd.Size = new System.Drawing.Size(202, 32);
             this.storageButtonAdd.TabIndex = 10;
             this.storageButtonAdd.Text = "Добавить склад";
             this.storageButtonAdd.UseVisualStyleBackColor = true;
@@ -130,10 +141,9 @@ namespace EdMap {
             // 
             // clearButton
             // 
-            this.clearButton.Location = new System.Drawing.Point(2, 498);
-            this.clearButton.Margin = new System.Windows.Forms.Padding(2);
+            this.clearButton.Location = new System.Drawing.Point(3, 584);
             this.clearButton.Name = "clearButton";
-            this.clearButton.Size = new System.Drawing.Size(136, 21);
+            this.clearButton.Size = new System.Drawing.Size(202, 32);
             this.clearButton.TabIndex = 9;
             this.clearButton.Text = "Очистить";
             this.clearButton.UseVisualStyleBackColor = true;
@@ -141,10 +151,9 @@ namespace EdMap {
             // 
             // addButton
             // 
-            this.addButton.Location = new System.Drawing.Point(2, 40);
-            this.addButton.Margin = new System.Windows.Forms.Padding(2);
+            this.addButton.Location = new System.Drawing.Point(3, 62);
             this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(136, 21);
+            this.addButton.Size = new System.Drawing.Size(202, 32);
             this.addButton.TabIndex = 8;
             this.addButton.Text = "Добавить точку";
             this.addButton.UseVisualStyleBackColor = true;
@@ -153,65 +162,64 @@ namespace EdMap {
             // distanceLable
             // 
             this.distanceLable.AutoSize = true;
-            this.distanceLable.Location = new System.Drawing.Point(69, 25);
-            this.distanceLable.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.distanceLable.Location = new System.Drawing.Point(104, 38);
             this.distanceLable.Name = "distanceLable";
-            this.distanceLable.Size = new System.Drawing.Size(13, 13);
+            this.distanceLable.Size = new System.Drawing.Size(18, 20);
             this.distanceLable.TabIndex = 7;
             this.distanceLable.Text = "0";
             // 
             // treeView1
             // 
-            this.treeView1.Location = new System.Drawing.Point(2, 199);
-            this.treeView1.Margin = new System.Windows.Forms.Padding(2);
+            this.treeView1.Location = new System.Drawing.Point(3, 270);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(137, 294);
+            this.treeView1.Size = new System.Drawing.Size(202, 308);
             this.treeView1.TabIndex = 6;
             this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(2, 25);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(3, 38);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.Size = new System.Drawing.Size(94, 20);
             this.label3.TabIndex = 5;
             this.label3.Text = "Растояние:";
             // 
             // getPathButton
             // 
-            this.getPathButton.Location = new System.Drawing.Point(2, 2);
-            this.getPathButton.Margin = new System.Windows.Forms.Padding(2);
+            this.getPathButton.Location = new System.Drawing.Point(3, 3);
             this.getPathButton.Name = "getPathButton";
-            this.getPathButton.Size = new System.Drawing.Size(136, 21);
+            this.getPathButton.Size = new System.Drawing.Size(202, 32);
             this.getPathButton.TabIndex = 0;
             this.getPathButton.Text = "Посчитать";
             this.getPathButton.UseVisualStyleBackColor = true;
             this.getPathButton.Click += new System.EventHandler(this.getPathButton_Click);
             // 
-            // label2
+            // loadPanel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 152);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 13);
-            this.label2.TabIndex = 14;
-            this.label2.Text = "label";
+            this.loadPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.loadPanel.Controls.Add(this.label2);
+            this.loadPanel.Controls.Add(this.progressBar1);
+            this.loadPanel.Location = new System.Drawing.Point(14, 207);
+            this.loadPanel.Name = "loadPanel";
+            this.loadPanel.Size = new System.Drawing.Size(210, 70);
+            this.loadPanel.TabIndex = 15;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(792, 538);
+            this.ClientSize = new System.Drawing.Size(1308, 958);
+            this.Controls.Add(this.loadPanel);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.gMapControl1);
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.LoadCapacityNUD)).EndInit();
+            this.loadPanel.ResumeLayout(false);
+            this.loadPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -231,6 +239,7 @@ namespace EdMap {
         private System.Windows.Forms.NumericUpDown LoadCapacityNUD;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel loadPanel;
     }
 }
 
